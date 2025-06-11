7afb83c0429a43bd835ce440fac4afbe396fe792
Chintankumar5799 <chintanbhalodia5@gmail.com>
2025-06-11 17:08:13 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/SpringJnknDkrProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJnknDkrProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJnknDkrProjectApplication.class, args);
+		System.out.println("Java, Jenkin and docker");
+	}
+
+}
