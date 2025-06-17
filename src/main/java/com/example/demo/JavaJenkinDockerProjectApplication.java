2fcc4c723b061e3031970569aa1225b9329634fa
Chintankumar5799 <chintanbhalodia5@gmail.com>
2025-06-17 16:52:59 +0530

    Third COmmit

--- src/main/java/com/example/demo/JavaJenkinDockerProjectApplication.java
+++ src/main/java/com/example/demo/JavaJenkinDockerProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJnknDkrProjectApplication {
+public class JavaJenkinDockerProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJnknDkrProjectApplication.class, args);
+		SpringApplication.run(JavaJenkinDockerProjectApplication.class, args);
 		System.out.println("Java, Jenkin and docker");
 	}
 }
--- src/test/java/com/example/demo/JavaJenkinDockerProjectApplicationTests.java
+++ src/test/java/com/example/demo/JavaJenkinDockerProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringJnknDkrProjectApplicationTests {
+class JavaJenkinDockerProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
